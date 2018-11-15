56c08d18f9b2e8febf7f9b3342c799b5b269bb27
Tamas Kiss <sauriaster@gmail.com>
2018-11-15 10:47:04 +0100

    init commit, added perm array + generation for perm array + fase method

---
+++ NoiseGenerator1/Noise.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace noise
+{
+    class Perlin
+    {
+        const ushort PERMARRAYSIZE = 512;  // should be 2^n
+        private static readonly int[] perm = new int[PERMARRAYSIZE];
+
+        public Perlin()
+        {
+            FillPermArray();
+        }
+
+        private double Fade(double t)
+        {
+            return Math.Pow(t, 3) * (t * (t * 6 - 15) + 10);  // 6t^5 - 15t^4 + 10t^3
+        }
+
+
+        private void FillPermArray()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < PERMARRAYSIZE; i++)
+            {
+                perm[i] = rnd.Next(0, 255);
+            }
+        }
+
+        public int[] GetPermArray() { return perm; }
+
+
+    }
+
+
+}
---
+++ NoiseGenerator1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using noise;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Perlin core = new Perlin();
+        int[] test = core.GetPermArray();
+        foreach(int element in test)
+        {
+            Console.Write(element + " ");
+
+        }
+        Console.Read();
+       
+    }
+}
+
