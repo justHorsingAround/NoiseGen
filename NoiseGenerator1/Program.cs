09344071db0e5e7068c48337751a42292cf5c156
Tamas Kiss <sauriaster@gmail.com>
2018-11-16 12:18:19 +0100

    testing C++ dll reach successfully

--- NoiseGenerator1/Program.cs
+++ NoiseGenerator1/Program.cs
@@ -1,18 +1,23 @@
 ﻿using noise;
+using System.Runtime.InteropServices;
 
 class Program
 {
     public static void Main(string[] args)
     {
+        main();
+
         const int IMAGE_WIDTH = 512;
         const int IMAGE_HEIGTH = IMAGE_WIDTH;
         double FREQUENCY_X = 4;
         double FREQUENCY_Y = FREQUENCY_X;
         const string OUTPUT_FILE = "C:/Users/Stable-tec/Pictures/test.png";
 
-        IController generator = new Controller(IMAGE_WIDTH, IMAGE_HEIGTH, FREQUENCY_X, FREQUENCY_Y, OUTPUT_FILE);
-        generator.Run();
+        //IController generator = new Controller(IMAGE_WIDTH, IMAGE_HEIGTH, FREQUENCY_X, FREQUENCY_Y, OUTPUT_FILE);
+        //generator.Run();
+    }
 
-    }       
+    [DllImport("cpp4.dll", EntryPoint = "main")]
+    public static extern void main();
 }
 
