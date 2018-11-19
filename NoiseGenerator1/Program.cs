639b408c7a281da76e300f1cb0cb2c2ce3a6e95a
Tamas Kiss <sauriaster@gmail.com>
2018-11-19 10:53:50 +0100

    added test reach of the cpp dll

--- NoiseGenerator1/Program.cs
+++ NoiseGenerator1/Program.cs
@@ -5,8 +5,6 @@ class Program
 {
     public static void Main(string[] args)
     {
-        main();
-
         const int IMAGE_WIDTH = 512;
         const int IMAGE_HEIGTH = IMAGE_WIDTH;
         double FREQUENCY_X = 4;
@@ -15,9 +13,17 @@ class Program
 
         //IController generator = new Controller(IMAGE_WIDTH, IMAGE_HEIGTH, FREQUENCY_X, FREQUENCY_Y, OUTPUT_FILE);
         //generator.Run();
+
+        int t = 5;
+        test1(t);
+        main();
+        
     }
 
-    [DllImport("cpp4.dll", EntryPoint = "main")]
+    [DllImport("../Visualizer/Visualizer.dll", EntryPoint = "main")]
     public static extern void main();
+
+    [DllImport("../Visualizer/Visualizer.dll", CallingConvention = CallingConvention.Cdecl)]
+    public static extern void test1(int t);
 }
 
